71189ff5dfeb2859dc4abedb2d3810986cc5ca3f
shamimsikder <shamimsikder138@gmail.com>
2023-05-19 22:52:53 +0600

    fixed modal issues

--- src/Pages/Home/ShopByCategory/ShopByCategory.jsx
+++ src/Pages/Home/ShopByCategory/ShopByCategory.jsx
@@ -41,15 +41,16 @@ const ShopByCategory = () => {
                                         <h3 className="text-lg font-bold mb-2">{toy.name}</h3>
                                         <p className="text-gray-600 mb-2">{toy.price}</p>
                                         <p className="text-gray-600 mb-2">Rating: {toy.rating}</p>
-                                        <label className="bg-[#65C3C8] hover:bg-[#529EA9] w-full text-white px-4 py-2 rounded-md text-center cursor-pointer" htmlFor="my-modal-3" data-aos="fade-right" data-aos-duration="500">
+                                        <label className="bg-[#65C3C8] hover:bg-[#529EA9] w-full text-white px-4 py-2 rounded-md text-center cursor-pointer"  htmlFor={`my-modal-${toy.id}`} data-aos="fade-right" data-aos-duration="500">
                                             View Details
                                         </label>
-                                        <input type="checkbox" id="my-modal-3" className="modal-toggle" />
+                                        <input type="checkbox" id={`my-modal-${toy.id}`} className="modal-toggle" />
                                         <div className="modal">
-                                        <div className="modal-box relative">
-                                            <label htmlFor="my-modal-3" className="btn btn-primary text-white btn-sm btn-circle absolute right-2 top-2">✕</label>
+                                        <div className="modal-box relative flex items-center flex-col">
+                                            <label htmlFor={`my-modal-${toy.id}`} className="btn btn-primary text-white btn-sm btn-circle absolute right-2 top-2">✕</label>
+                                            <img src={toy.picture} alt={toy.name} className="w-96 h-96 rounded-md object-cover mb-4"/>
                                             <h3 className="text-lg font-bold">Congratulations random Internet user!</h3>
-                                            <p className="py-4">You've been selected for a chance to get one year of subscription to use Wikipedia for free!</p>
+                                            <p className="py-4 text-center">You've been selected for a chance to get one year of subscription to use Wikipedia for free!</p>
                                         </div>
                                         </div>
                                     </div>
